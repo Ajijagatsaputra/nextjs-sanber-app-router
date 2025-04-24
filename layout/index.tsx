d762cabf07bc307e137fbf256d0c29e2aaf176d7
Ajijagatsaputra <ajijagat90@gmail.com>
2025-04-24 21:50:59 +0700

    Tugas - hari 4 Layouting & Styling (AppRouter)

---
+++ layout/index.tsx
@@ -0,0 +1,35 @@
+// import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <header className="bg-blue-600 text-white p-4 ">
+          <div className="container mx-auto flex justify-between items-center">
+            <h1 className="text-xl font-bold ">Next Js App</h1>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+        <main className="flex-1 container mx-auto p-4">{children}</main>
+        <footer className="bg-gray-800 text-white p-4 text-center">
+          <p>&copy; {new Date().getFullYear()} My Personal Website</p>
+        </footer>
+      </div>
+    </>
+  )
+}
